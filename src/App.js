63ad00ab123bbd27799d9f0e5bcc41b03121710d
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-05 13:07:51 -0600

    hfhg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles/App.css";
 function App() {
   return (
     <div className="app">
-      <<h1 className="title">¡Bienvenido a Sapiens IA Laboratories!</h1>
+      <h1 className="title">¡Bienvenido a Sapiens IA Laboratories!</h1>
       <p className="description">Aquí podrás encontrar algunos demos y modelos de Inteligencia Artificial desarrollados por nuestro equipo de científicos. Inicia sesión para probarlos y experimentar con ellos.</p>
       <Button variant="contained" color="primary" className="cta" href="/demos">
         Ver Demos

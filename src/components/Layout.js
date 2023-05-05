7f2fc3add16dd5a27cd93a023e00579eec445cbd
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-05 12:10:33 -0600

    1234

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { useUser } from "@clerk/clerk-react";
 import Header from "./Header";
-import Dashboard from "./Dashboard";
 
 function Layout() {
-  const user = useUser();
+  return (
+    <>
+      <Header />
 
-  if (user) {
-    return (
-      <>
-        <Header />
-        <Dashboard />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Header />
-        <Outlet />
-      </>
-    );
-  }
+      <Outlet />
+    </>
+  );
 }
 
 export default Layout;

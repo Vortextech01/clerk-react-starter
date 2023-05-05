45005633644ade3c4f6841ebf23bfce7e658cd00
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-05-05 13:06:41 -0600

    123

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import logo from "./logoepic.gif";
 import "./styles/App.css";
-import { Button } from '@material-ui/core';
-
-import ClerkLoginButton from './components/ClerkLoginButton';
 
 function App() {
   return (
     <div className="app">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Sapiens IA Laboratories</Typography>
-          <ClerkLoginButton />
-        </Toolbar>
-      </AppBar>
-      <img src={logo} alt="Sapiens IA Laboratories Logo" className="logo" />
-      <h1 className="title">¡Bienvenido a Sapiens IA Laboratories!</h1>
+      <<h1 className="title">¡Bienvenido a Sapiens IA Laboratories!</h1>
       <p className="description">Aquí podrás encontrar algunos demos y modelos de Inteligencia Artificial desarrollados por nuestro equipo de científicos. Inicia sesión para probarlos y experimentar con ellos.</p>
       <Button variant="contained" color="primary" className="cta" href="/demos">
         Ver Demos
